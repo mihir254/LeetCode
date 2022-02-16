645df13c2e12cb30546b674c00972b814232032d
Mihir Bhansali <mbmihirbhansali@gmail.com>
2022-02-16 02:19:06 -0500

    Easy
    Runtime: 40 ms, faster than 99.47% of Python3 online submissions for N-ary Tree Preorder Traversal.
    Memory Usage: 16.1 MB, less than 36.51% of Python3 online submissions for N-ary Tree Preorder Traversal.

---
+++ 589-N-ary-Tree-Preorder-Traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root):
+        if not root:
+            return []
+        res, stack = [], [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            stack.extend(node.children[::-1])
+        return res
