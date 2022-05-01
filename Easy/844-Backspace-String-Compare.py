5eb60456bd6f6cfe5f058423618e8636a43983f8
Mihir Bhansali <mbmihirbhansali@gmail.com>
2022-04-30 22:09:59 -0400

    stack

---
+++ Easy/844-Backspace-String-Compare.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1, stack2 = [], []
+        for i in s:
+            if i != "#":
+                stack1.append(i)
+            else:
+                if stack1:
+                    stack1.pop()
+        for i in t:
+            if i != "#":
+                stack2.append(i)
+            else:
+                if stack2:
+                    stack2.pop()
+        return True if "".join(stack1)=="".join(stack2) else False
