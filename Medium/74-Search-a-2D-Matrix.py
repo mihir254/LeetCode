4fba2a0007dc05d8701d91b93ef95ca434a41e86
Mihir Bhansali <mbmihirbhansali@gmail.com>
2023-08-07 17:12:55 -0400

    Binary Search

--- Medium/74-Search-a-2D-Matrix.py
+++ Medium/74-Search-a-2D-Matrix.py
@@ -1,18 +1,30 @@
 class Solution:
     def searchMatrix(self, matrix, target):
-        row = 0
-        while row < len(matrix)-1:
-            if matrix[row][-1] >= target:
-                break
-            row += 1
-        arr = matrix[row]
-        l, r = 0, len(arr)-1
-        while l <= r:
-            mid = (l+r)//2
-            if target == arr[mid]:
+        top, bottom = 0, len(matrix)-1
+        mainRow = -1
+        while top <= bottom:
+            center = (top+bottom) // 2
+            if matrix[center][0] == target:
                 return True
-            if arr[mid] < target:
-                l = mid + 1
+            if matrix[center][0] < target:
+                if matrix[center][-1] == target:
+                    return True
+                if matrix[center][-1] > target:
+                    mainRow = center
+                    break
+                else:
+                    top = center + 1
+            elif matrix[center][0] > target:
+                bottom = center - 1
+        searchArea = matrix[mainRow]
+        print(matrix[mainRow])
+        left, right = 0, len(matrix[mainRow]) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if matrix[mainRow][mid] == target:
+                return True
+            if matrix[mainRow][mid] < target:
+                left = mid + 1
             else:
-                r = mid - 1
+                right = mid - 1
         return False
